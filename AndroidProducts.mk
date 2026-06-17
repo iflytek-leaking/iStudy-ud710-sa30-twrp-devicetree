46f2608965493226905dfa89c15864a914527d44
Ramdisk01 <bwrp17001499@outlook.com>
2026-06-17 14:30:14 +0800

    Add: Basic tree
    The basic tree file is generated online using Twrpdtgen. For details on the process, see https://github.com/Manatsu0721/action_build_twrp_device_tree/actions/runs/27668219116/job/81826951087 .

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_ums9620_se50.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_ums9620_se50-user \
+    omni_ums9620_se50-userdebug \
+    omni_ums9620_se50-eng
---
+++ README.md
@@ -0,0 +1,10 @@
+# Android device tree for iFLYTEK ums9620_se50_s30pro (ums9620_se50)
+
+```
+#
+# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
